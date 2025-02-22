d432178bfa2bfffd484313def6095a4a944536d4
lukas <mindrunner@users.noreply.github.com>
2025-02-22 22:56:32 +0100

    chore: new fleets for lu

--- src/main/basedbot/fleet-strategies/mainnet-lu-strategy.ts
+++ src/main/basedbot/fleet-strategies/mainnet-lu-strategy.ts
@@ -30,7 +30,7 @@ export const mainnetLuStrategy = (
                 },
             ],
             [
-                'Mouse',
+                'Mouse Fleet',
                 {
                     fleet: null,
                     strategy: createMiningStrategy(
