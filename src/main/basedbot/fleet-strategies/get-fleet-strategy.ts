52c73e1793f3aed9eff8c53c86aec8883fc1281c
lukas <mindrunner@users.noreply.github.com>
2025-03-11 23:04:02 +0100

    fix: fix mining determinism

--- src/main/basedbot/fleet-strategies/get-fleet-strategy.ts
+++ src/main/basedbot/fleet-strategies/get-fleet-strategy.ts
@@ -4,7 +4,7 @@ import { Player } from '../lib/sage/state/user-account'
 import { WorldMap } from '../lib/sage/state/world-map'
 
 import { atlasnetLuStrategy } from './atlasnet-lu-strategy'
-import { disbandAllStrategy } from './disband-all-strategy'
+import { destructAllStrategy } from './destruct-all-strategy'
 import { mainnetGellsnStrategy } from './mainnet-gellsn-strategy'
 import { mainnetLuStrategy } from './mainnet-lu-strategy'
 import { StrategyConfig } from './strategy-config'
@@ -23,19 +23,19 @@ export const getFleetStrategy = (
             return mainnetLuStrategy(map, player, game)
         case 'CgHvzwGbwWv3CwLTvEgeqSKeD8EwMdTfiiCG3dFrKVVC':
             // return atlasnetFcStrategy(150)(map, player, game, 'mud')
-            return disbandAllStrategy(map, player, game)
+            return destructAllStrategy(map, player, game)
         case '9KBrgWVjsmdZ3YEjcsa3wrbbJREgZgS7vDbgoz2aHaNm':
             // return atlasnetFcStrategy(150)(map, player, game, 'ustur')
-            return disbandAllStrategy(map, player, game)
+            return destructAllStrategy(map, player, game)
         case 'FUwHSqujzcPD44SDZYJXuk73NbkEyYQwcLMioHhpjbx2':
             // return atlasnetFcStrategy(150)(map, player, game, 'oni')
-            return disbandAllStrategy(map, player, game)
+            return destructAllStrategy(map, player, game)
         case '34ghznSJCYEMrS1aC55UYZZUuxfuurA9441aKnigmYyz':
             // return atlasnetQtStrategy(1)(map, player, game, 'le.local')
-            return disbandAllStrategy(map, player, game)
+            return destructAllStrategy(map, player, game)
         case '3wGtzSseukZ7ZAdAgRnhgP34QiLx1VWWALBsr9FYShXu':
             // return atlasnetQtStrategy(1)(map, player, game, 'whosthis')
-            return disbandAllStrategy(map, player, game)
+            return destructAllStrategy(map, player, game)
         default:
             throw new Error('Unknown strategy')
     }
